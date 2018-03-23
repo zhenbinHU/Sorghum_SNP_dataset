711c312d9db09f6f8342eb7f6ba95ab1f0c2b897
Zhenbin <zhenbinhu@gmail.com>
2018-03-23 14:14:51 -0500

    Update Figures.R

--- Figures.R
+++ Figures.R
@@ -17,3 +17,21 @@ is=1.5)
  }
 
 
+# LD heatmmap plot using LDheatmap R package 
+# Using Y1 gene as example
+# Color from blue to red mean R^2 from 0 to 1
+
+library(LDheatmap)
+setwd("~/Desktop/heatmap")
+load("/Users/zhenbin/Desktop/heatmap/snp_dis_y1.1.rda")
+load("/Users/zhenbin/Desktop/heatmap/snp_data_y1.rda")
+png("heatmap_Y1.png",width=6,height=6,res=600,units="in")
+rgb.palette <- colorRampPalette(rev(c("blue", "orange", "red")), space = "rgb")
+MyHeatmap<-LDheatmap(snp_data,snp_dis,flip=TRUE,color=rgb.palette(20),title="",name = "myLDgrob",SNP.name="S2_57610965")
+LD.grob1 <- editGrob(MyHeatmap$LDheatmapGrob, gPath("heatMap", "title"), gp = gpar(cex = 1.25, col = "blue"))
+LD.grob2 <- editGrob(LD.grob1, gPath("Key","title"), gp = gpar(cex = 1.5, col = "black"))
+LD.grob3 <- editGrob(LD.grob2, gPath("Key", "labels"),gp = gpar(cex = 1.3, col = "black"))
+grid.newpage() 
+grid.draw(LD.grob3)
+dev.off()
+
