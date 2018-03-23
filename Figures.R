c5edca1479d23e8eb9eb116d80e09c8f46016f3c
Zhenbin <zhenbinhu@gmail.com>
2018-03-23 14:11:05 -0500

    Update Figures.R

--- Figures.R
+++ Figures.R
@@ -1,1 +1,19 @@
+# manhattan plot was draw using qqman packages
+setwd("")
+list<-list.files()
+for(i in list){
+   setwd(paste0("/bulk/zhenbin/Sorghum_diversity/GWAS/old/",i))
+   tmp<-list.files(pattern="GWAS.Results.csv")
+   tmp<-read.csv(tmp,header=T)
+   nobs=unique(tmp$nobs)
+   tmp<-tmp[tmp$maf>0.05,1:5]
+   names(tmp)<-c("SNP","CHR","BP","P","maf")
+   png(paste0("/bulk/zhenbin/Sorghum_diversity/GWAS/Figures/",i,".png"),width=12,height=6,type="cairo",res=600,units="in")
+       par(mai=c(1,1.2,0.5,0.5))
+       manhattan(tmp,suggestiveline = F, genomewideline = -log10(0.05/dim(tmp)[1]),ylim=c(0,max(-log10(tmp$P))+3), cex.lab=2,cex.ax
+is=1.5)
+       dev.off()
+   print(paste0("Pop size for trait:", i," is: ",nobs))
+ }
+
 
