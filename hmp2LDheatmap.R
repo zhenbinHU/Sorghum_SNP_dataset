b0b6f846a70ee0778c18b040fb3e79717d0f0987
Zhenbin <zhenbinhu@gmail.com>
2018-08-02 11:26:12 -0500

    Update hmp2LDheatmap.R

--- hmp2LDheatmap.R
+++ hmp2LDheatmap.R
@@ -29,3 +29,21 @@ hmp2LDheatmap<-function(hapmap,out=""){
 save(snp_data,file=paste0("snp_data_",out,".rda"))
 save(snp_dis,file=paste0("snp_dis_",out,".rda"))
 }
+
+# LD heatmmap plot using LDheatmap R package 
+# Using Y1 gene as example
+# Color from blue to red mean R^2 from 0 to 1
+hmp2LDheatmap(y1_hapmap,out="y1") # y1_hapmap is the regional SNP data based on y1 gene.
+
+library(LDheatmap)
+load("snp_dis_y1.1.rda")
+load("snp_data_y1.rda")
+png("heatmap_Y1.png",width=6,height=6,res=600,units="in",type="cairo")
+rgb.palette <- colorRampPalette(rev(c("blue", "orange", "red")), space = "rgb")
+MyHeatmap<-LDheatmap(snp_data,snp_dis,flip=TRUE,color=rgb.palette(20),title="",name = "myLDgrob",SNP.name="S2_57610965")
+LD.grob1 <- editGrob(MyHeatmap$LDheatmapGrob, gPath("heatMap", "title"), gp = gpar(cex = 1.25, col = "blue"))
+LD.grob2 <- editGrob(LD.grob1, gPath("Key","title"), gp = gpar(cex = 1.5, col = "black"))
+LD.grob3 <- editGrob(LD.grob2, gPath("Key", "labels"),gp = gpar(cex = 1.3, col = "black"))
+grid.newpage() 
+grid.draw(LD.grob3)
+dev.off()
